d956c2f65e63c574a8fd5f85594943fb23b5e33b
Adam <galekxy13@hotmail.com>
2021-06-07 15:25:51 -0400

    fixed nav menu links

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -6,7 +6,31 @@ export default function Menu({ headerState, setHeaderState }) {
     // https://stackoverflow.com/questions/42253277/react-router-v4-how-to-get-current-route
     const location = useLocation();
     // console.log(location.pathname);
-    if (location.pathname === "/portfolioItem"){
+    if (location.pathname === "/"){
+        return (
+            <div className={"menu " +(headerState && "active")}>
+                <ul>
+                    
+                    {/* could create a component for these li's, something to improve for next version */}
+                    <li onClick={()=>setHeaderState(false)}>
+                        <a href="#intro">Home</a>
+                    </li>
+                    <li onClick={()=>setHeaderState(false)}>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li onClick={()=>setHeaderState(false)}>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li onClick={()=>setHeaderState(false)}>
+                        <a href="#testimonials">Testimonials</a>
+                    </li>
+                    <li onClick={()=>setHeaderState(false)}>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    } else {
         return (
             <div className={"menu " +(headerState && "active")}>
                 <ul>
@@ -30,31 +54,6 @@ export default function Menu({ headerState, setHeaderState }) {
                     <li onClick={()=>setHeaderState(false)}>
                         <a href="#contact">Contact</a>
                     </li> */}
-                </ul>
-            </div>
-        )
-    } else {
-        return (
-
-            <div className={"menu " +(headerState && "active")}>
-                <ul>
-                    
-                    {/* could create a component for these li's, something to improve for next version */}
-                    <li onClick={()=>setHeaderState(false)}>
-                        <a href="#intro">Home</a>
-                    </li>
-                    <li onClick={()=>setHeaderState(false)}>
-                        <a href="#portfolio">Portfolio</a>
-                    </li>
-                    <li onClick={()=>setHeaderState(false)}>
-                        <a href="#experience">Experience</a>
-                    </li>
-                    <li onClick={()=>setHeaderState(false)}>
-                        <a href="#testimonials">Testimonials</a>
-                    </li>
-                    <li onClick={()=>setHeaderState(false)}>
-                        <a href="#contact">Contact</a>
-                    </li>
                 </ul>
             </div>
         )
